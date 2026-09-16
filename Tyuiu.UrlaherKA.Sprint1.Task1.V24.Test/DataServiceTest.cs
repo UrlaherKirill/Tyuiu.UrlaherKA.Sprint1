806565bf0f1d4914fd213b71d4554801a3a06540
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-16 18:44:01 +0500

    Task1 and fix sample in Task0 console app

---
+++ Tyuiu.UrlaherKA.Sprint1.Task1.V24.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.UrlaherKA.Sprint1.Task1.V24.Lib;
+namespace Tyuiu.UrlaherKA.Sprint1.Task1.V24.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void Example()
+        {
+            double x, y;
+            x = 9.0;
+            y = 2.0;
+            var res = ds.Calculate(x,y);
+            Assert.AreEqual(-2, res);
+        }
+    }
+}
--- Tyuiu.UrlaherKA.Sprint1.Task1.V24/Program.cs
+++ Tyuiu.UrlaherKA.Sprint1.Task1.V24/Program.cs
@@ -1,36 +1,41 @@
-﻿using Tyuiu.UrlaherKA.Sprint1.Task0.V1.Lib;
-
-namespace Tyuiu.UrlaherKA.Sprint1.Task0.V1
+﻿using Tyuiu.UrlaherKA.Sprint1.Task1.V24.Lib;
+namespace Tyuiu.UrlaherKA.Sprint1.Task1.V24
 {
     class Program
     {
         static void Main(string[] args)
         {
             DataService ds = new DataService();
-            
             Console.Title = "Спринт #1 | Выполнил: Урлахер К. А. | ПКТб-26-1";
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #1                                                               *");
             Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
-            Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #24                                                              *");
             Console.WriteLine("* Выполнил: Урлахер Кирилл Александрович | ПКТб-26-1                      *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу, которая вычисляет выражение 36/2/9+1-6/2*3          *");
-            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле (1-x)/(2+y) и печатает его на экране.    *");
             Console.WriteLine("*                                                                         *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* 36/2/9+1-6/2*3                                                          *");
+
+
+            Console.WriteLine("Введите значение x:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение y");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            var res = ds.Calculate(x, y);
+
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine(ds.Calculate());
-            Console.ReadLine();
+            Console.WriteLine(res);
         }
     }
 }
