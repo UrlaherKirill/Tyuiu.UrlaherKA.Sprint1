8c6bacc643a5831385dfb63c371f2080213e6bae
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-16 16:06:55 +0500

    Add project files.

---
+++ Tyuiu.UrlaherKA.Sprint1.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.UrlaherKA.Sprint1.Task0.V1.Lib;
+namespace Tyuiu.UrlaherKA.Sprint1.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Example()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(-6, res);
+        }
+    }
+}
