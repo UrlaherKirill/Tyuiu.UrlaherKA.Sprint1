4b0925f5961f1987f2f1f62ee6c16083b9130558
UrlaherKa <kirillurlaher882@gmail.com>
2026-09-16 20:06:55 +0500

    Task2

---
+++ Tyuiu.UrlaherKA.Sprint1.Task2.V5.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.UrlaherKA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.UrlaherKA.Sprint1.Task2.V5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        DataService ds = new DataService();
+        [TestMethod]
+        public void SquareCube()
+        {
+            int x = 5;
+            int Square = ds.CalculateSideSquare(x);
+            Assert.AreEqual(25, Square);
+        }
+    }
+}
---
+++ Tyuiu.UrlaherKA.Sprint1.Task2.V5/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.UrlaherKA.Sprint1.Task2.V5.Lib;
+namespace Tyuiu.UrlaherKA.Sprint1.Task2.V5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #1 | Выполнил: Урлахер К. А. | ПКТб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #5                                                              *");
+            Console.WriteLine("* Выполнил: Урлахер Кирилл Александрович | ПКТб-26-1                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите длину стороны куба:");
+            int x = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.CalculateSideSquare(x);
+            Console.WriteLine($"Площадь поверхности куба = {res}");
+            Console.ReadKey();
+        }
+    }
+}
